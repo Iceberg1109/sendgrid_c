32a8b858502b0edc48cc2036c83192aa50fee8f1
Iceberg1109 <smartdev1109@outlook.com>
2019-12-12 13:06:58 +0800

    done

---
+++ SendGrid/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SendGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_send = new System.Windows.Forms.Button();
+            this.tb_email = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(184, 147);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(168, 39);
+            this.btn_send.TabIndex = 0;
+            this.btn_send.Text = "Send an Email";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // tb_email
+            // 
+            this.tb_email.Location = new System.Drawing.Point(116, 99);
+            this.tb_email.Name = "tb_email";
+            this.tb_email.Size = new System.Drawing.Size(289, 20);
+            this.tb_email.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 302);
+            this.Controls.Add(this.tb_email);
+            this.Controls.Add(this.btn_send);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.TextBox tb_email;
+    }
+}
+
---
+++ SendGrid/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace SendGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        async private void btn_send_Click(object sender, EventArgs e)
+        {
+            string subject = "Instagram weekly report";
+            string contect = File.ReadAllText("index.html");
+            string from = "support@omni.com";
+            string apiName = "";
+            string apiKey = "";
+
+            await SendGridEmail(subject, contect, from, new string[] { "a@a.com" }, apiName, apiKey);
+        }
+
+        public async Task SendGridEmail(string subject, string content, string from, string[] to, string sName, string sKey)
+        {
+            var apiKey = Environment.GetEnvironmentVariable(sName);
+            var client = new SendGridClient(sKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(from, from),
+                Subject = subject,
+                PlainTextContent = content,
+                HtmlContent = content
+            };
+            foreach (var item in to)
+            {
+                msg.AddTo(new EmailAddress(item, item));
+            }
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
